5e34e0920691c814ce4b5a1dac77ae90a7766f46
Nikko Adrian <nikkoadrian02@gmail.com>
2025-07-31 19:26:47 +0700

    Add PKL request letter template and related assets
    Added a new Blade template for the PKL (Praktik Kerja Lapangan) request letter, including a custom CSS file for document styling and new image assets for signatures and logos. Updated the dikdasmenmuh.png image. These changes support the generation of formal PKL request documents for SMK Muhammadiyah Kandanghaur.

---
+++ resources/views/partials/docx/permohonan.blade.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Surat Permohonan PKL</title>
+    <link rel="stylesheet" href="{{ asset('assets/dist/css/styles_docx.css') }}" />
+  </head>
+  <body>
+    <!-- Halaman 1 -->
+    <div class="page">
+      <div style="text-align: center">
+        <table style="width: 100%; border-collapse: collapse">
+          <tr>
+            <td style="width: 100px" align="center">
+              <img src="{{ asset('assets/dist/img/dikdasmenmuh.png') }}" width="100" />
+            </td>
+            <td align="center">
+              <div style="font-size: 10pt; font-weight: bold; color: #007bff">
+                MAJELIS PENDIDIKAN DASAR DAN MENENGAH PENDIDIKAN NONFORMAL
+              </div>
+              <div style="font-size: 10pt; font-weight: bold; color: #007bff">
+                PIMPINAN WILAYAH MUHAMMADIYAH JAWA BARAT
+              </div>
+              <div style="font-size: 18pt; font-weight: bold; color: #007bff">
+                SMK MUHAMMADIYAH KANDANGHAUR
+              </div>
+              <div style="font-size: 12pt; font-weight: bold">
+                Terakreditasi “A” (Unggul)
+              </div>
+              <div style="font-size: 12pt; font-weight: bold">
+                Nomor : 18572022/BAN-SM/SK/2022
+              </div>
+              <div style="font-size: 8pt; margin-top: 5px">
+                Konsentrasi Keahlian : Teknik Elektronika Industri, Teknik
+                Pengelasan, Teknik Kendaraan Ringan, <br />
+                Teknik Komputer dan Jaringan, Teknik Sepeda Motor, Layanan
+                Penunjang Kefarmasian Klinis dan Komunitas
+              </div>
+              <div style="font-size: 8pt">
+                Jl. Raya Karanganyar No. 28/A Kec. Kandanghaur Kab. Indramayu
+                45254 Telp. (0234) 507239 <br />
+                email : smkmuhkdh@gmail.com website :
+                https://www.smkmuhkandanghaur.sch.id
+              </div>
+            </td>
+            <td style="width: 100px" align="center">
+              <img src="{{ asset('assets/dist/img/logo.png') }}" width="80" />
+            </td>
+          </tr>
+        </table>
+
+        <!-- Dua garis horizontal khas -->
+        <hr
+          style="border: 2px solid black; margin-top: 4px; margin-bottom: 1px"
+        />
+        <hr style="border: 1px solid black; margin-top: 0" />
+      </div>
+
+      <div class="meta">
+        <table>
+          <tr>
+            <td>Nomor</td>
+            <td>: 631.001/III.4.AU/J/2025</td>
+          </tr>
+          <tr>
+            <td>Lamp</td>
+            <td>: 1 Lembar</td>
+          </tr>
+          <tr>
+            <td>Perihal</td>
+            <td><strong>: Permohonan Praktik Kerja Lapangan (PKL)</strong></td>
+          </tr>
+        </table>
+      </div>
+
+      <p>
+        Yth. Bapak/Ibu Pimpinan<br />
+        <strong>PT. Dharma Electrindo Manufacturing</strong><br />
+        di Tempat
+      </p>
+
+      <p>Dengan hormat,</p>
+
+      <p class="indent">
+        Sesuai dengan Undang-undang Peraturan Menteri Pendidikan dan Kebudayaan
+        Republik Indonesia Nomor 50 Tahun 2020 Tentang Praktik Kerja Lapangan
+        Bagi Peserta Didik, maka Program Kerja SMK Muhammadiyah Kandanghaur
+        Tahun Ajaran 2025/2026 khususnya mengenai pelaksanaan kegiatan
+        <strong>Praktik Kerja Lapangan (PKL)</strong> untuk siswa Tingkat XI
+        <strong>Konsentrasi Keahlian: Teknik Kendaraan Ringan</strong> yang
+        Insya Allah akan dilaksanakan pada:
+      </p>
+
+      <p>
+        Hari, Tanggal : <strong>1 September s.d 30 November 2025</strong><br />
+        Waktu Pelaksanaan : <strong>3 Bulan</strong>
+      </p>
+
+      <p class="indent">
+        Dalam upaya memberikan pengalaman kerja nyata bagi siswa dalam
+        pembentukan kompetensi secara utuh dan bermakna, terutama pembentukan
+        sikap kerja sesuai kebutuhan dunia kerja, kami mewajibkan siswa tingkat
+        XI untuk mengikuti Program PKL di Dunia Usaha/Industri.
+      </p>
+
+      <p class="indent">
+        Sehubungan dengan hal tersebut, kami
+        <strong>SMK Muhammadiyah Kandanghaur</strong> Kabupaten Indramayu
+        memohon kerja sama untuk menerima dan membimbing siswa kami dalam
+        pelaksanaan PKL.
+      </p>
+
+      <p>
+        Untuk konfirmasi kesediaan peserta didik PKL, Bapak/Ibu dapat
+        menghubungi panitia PKL: <strong>081322584428 (Rizky)</strong>.
+      </p>
+
+      <p>Kandanghaur, Agustus 2025</p>
+      <p>
+        Demikian permohonan kami. Atas perhatian dan kerja sama Bapak/Ibu, kami
+        ucapkan terima kasih.
+      </p>
+
+      <div class="signature-section">
+        <div class="signature">
+          Ketua PKL<br />
+          <img
+            src="{{ asset('assets/dist/img/nanang.jpeg') }}"
+            alt="Tanda tangan Ketua"
+            height="70"
+          /><br />
+          <strong>H. NANANG HADI M, S.T.</strong>
+        </div>
+        <div class="signature center">
+          Mengetahui,<br />
+          Kepala Sekolah<br />
+          <img
+            src="{{ asset('assets/dist/img/kepsek.jpeg') }}"
+            alt="Tanda tangan Kepsek"
+            height="80"
+          /><br />
+          <strong>H. AFANDI, S.Pd.</strong>
+        </div>
+        <div class="signature">
+          Sekretaris PKL<br /><br /><br /><br /><br />
+
+          <strong>RIZKY RAMADONA, S.T.</strong>
+        </div>
+      </div>
+    </div>
+
+    <!-- Halaman 2 -->
+    <div class="page">
+      <div style="text-align: center">
+        <table style="width: 100%; border-collapse: collapse">
+          <tr>
+            <td style="width: 100px" align="center">
+              <img src="images/dikdasmen.jpg" width="100" />
+            </td>
+            <td align="center">
+              <div style="font-size: 10pt; font-weight: bold; color: #007bff">
+                MAJELIS PENDIDIKAN DASAR DAN MENENGAH PENDIDIKAN NONFORMAL
+              </div>
+              <div style="font-size: 10pt; font-weight: bold; color: #007bff">
+                PIMPINAN WILAYAH MUHAMMADIYAH JAWA BARAT
+              </div>
+              <div style="font-size: 18pt; font-weight: bold; color: #007bff">
+                SMK MUHAMMADIYAH KANDANGHAUR
+              </div>
+              <div style="font-size: 12pt; font-weight: bold">
+                Terakreditasi “A” (Unggul)
+              </div>
+              <div style="font-size: 12pt; font-weight: bold">
+                Nomor : 18572022/BAN-SM/SK/2022
+              </div>
+              <div style="font-size: 8pt; margin-top: 5px">
+                Konsentrasi Keahlian : Teknik Elektronika Industri, Teknik
+                Pengelasan, Teknik Kendaraan Ringan, <br />
+                Teknik Komputer dan Jaringan, Teknik Sepeda Motor, Layanan
+                Penunjang Kefarmasian Klinis dan Komunitas
+              </div>
+              <div style="font-size: 8pt">
+                Jl. Raya Karanganyar No. 28/A Kec. Kandanghaur Kab. Indramayu
+                45254 Telp. (0234) 507239 <br />
+                email : smkmuhkdh@gmail.com website :
+                https://www.smkmuhkandanghaur.sch.id
+              </div>
+            </td>
+            <td style="width: 100px" align="center">
+              <img src="images/logo.png" width="80" />
+            </td>
+          </tr>
+        </table>
+
+        <!-- Dua garis horizontal khas -->
+        <hr
+          style="border: 2px solid black; margin-top: 4px; margin-bottom: 1px"
+        />
+        <hr style="border: 1px solid black; margin-top: 0" />
+      </div>
+
+      <div class="form-title">
+        FORM ISIAN KESEDIAAN<br />DUNIA USAHA / DUNIA INDUSTRI
+      </div>
+      <div class="form-subtitle">
+        PESERTA PRAKTIK KERJA LAPANGAN (PKL)<br />
+        SMK MUHAMMADIYAH KANDANGHAUR<br />
+        TAHUN AJARAN 2025 / 2026
+      </div>
+
+      <table class="form-table">
+        <tr>
+          <td colspan="2"><strong>A. DATA PERUSAHAAN / INSTANSI</strong></td>
+        </tr>
+        <tr>
+          <td>1. Nama Perusahaan / Instansi</td>
+          <td>: ____________________________________________</td>
+        </tr>
+        <tr>
+          <td>2. Nama Pimpinan</td>
+          <td>: ____________________________________________</td>
+        </tr>
+        <tr>
+          <td>3. Alamat Perusahaan</td>
+          <td>: ____________________________________________</td>
+        </tr>
+        <tr>
+          <td>4. Contact Person / No. HP</td>
+          <td>: ____________________________________________</td>
+        </tr>
+
+        <tr>
+          <td colspan="2">
+            <strong><br />B. DATA KEGIATAN PKL</strong>
+          </td>
+        </tr>
+        <tr>
+          <td>1. Jumlah Siswa yang diterima</td>
+          <td>: ___________ Orang</td>
+        </tr>
+        <tr>
+          <td>2. Tanggal Pelaksanaan PKL</td>
+          <td>: <strong>1 September s.d 30 November 2025</strong></td>
+        </tr>
+      </table>
+
+      <p style="text-align: right">Indramayu, 6 Agustus 2025</p>
+      <p style="text-align: right">Yang Menerima,</p>
+      <br /><br /><br />
+      <p style="text-align: right">__________________________</p>
+    </div>
+  </body>
+</html>
