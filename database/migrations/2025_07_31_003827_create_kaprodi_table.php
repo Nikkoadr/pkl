0b9f1c1e3fd5e73f905bdadf61a90042268e57ea
Nikko Adrian <nikkoadrian02@gmail.com>
2025-08-08 09:36:50 +0700

    Update 2025_07_31_003827_create_kaprodi_table.php

--- database/migrations/2025_07_31_003827_create_kaprodi_table.php
+++ database/migrations/2025_07_31_003827_create_kaprodi_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('kaprodi', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('kompetensi_keahlian_id')->constrained('kompetensi_keahlian')->onDelete('cascade');
             $table->string('keterangan')->nullable();
             $table->timestamps();
         });
