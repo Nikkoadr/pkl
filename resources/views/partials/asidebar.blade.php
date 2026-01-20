e2206262acce12d30adf7a292d136e61de3144fb
Nikko Adrian <nikkoadrian02@gmail.com>
2026-01-20 09:21:33 +0700

    Update asidebar.blade.php

--- resources/views/partials/asidebar.blade.php
+++ resources/views/partials/asidebar.blade.php
@@ -27,12 +27,14 @@
                             <p>Dashboard</p>
                         </a>
                     </li>
+                    @canany(['admin', 'prodi', 'guru_pembimbing'])
                     <li class="nav-item">
                         <a href="/home/sidang_pkl" class="nav-link {{ request()->is('home/sidang_pkl') ? 'active' : '' }}">
                             <i class="nav-icon fas fa-file-alt"></i>
                             <p>Sidang PKL</p>
                         </a>
                     </li>
+                    @endcanany
                     @can('admin')
                         <li class="nav-item {{ request()->is('home/tahun_ajaran','home/dudi','home/users','home/kelas') ? 'menu-open' : '' }}">
                             <a href="#" class="nav-link {{ request()->is('home/tahun_ajaran','home/dudi','home/users','home/kelas') ? 'active' : '' }}">
